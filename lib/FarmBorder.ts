4e4a33271959afe1767ddb4f00e111836da42aa4
dvtate <toast27@gmail.com>
2021-07-27 17:59:36 -0400

    table placement rules and stuff

--- lib/FarmBorder.ts
+++ lib/FarmBorder.ts
@@ -60,12 +60,67 @@ export abstract class FarmBorder {
         return FarmBorder.matchingSegmentDict[origin];
     }
 
+    /**
+     * Get border segments included in this region
+     */
+    abstract getSegments(): FarmBorderSegment[];
 
     /**
-     * Verify border contains this segment
-     * @param segment segment to check
+     * Segments which correspond to given direction, and matches
      */
-    abstract includes(segment: FarmBorderSegment): boolean;
+    static directionalSides = (() => {
+        // Start with segments for top right half of tile
+        const ret = [
+            [[FarmBorderSegment.TOP_TOP_LEFT, FarmBorderSegment.TOP_TOP_RIGHT]],
+            [[FarmBorderSegment.SIDE_TOP_RIGHT, FarmBorderSegment.SIDE_BOT_RIGHT]],
+        ];
+
+        // Add matching segment pairs
+        ret.forEach(bs => bs.push(bs[0].map(FarmBorder.matchingSegment)))
+
+        // Mirror
+        ret.push(...ret.map(bs => bs.map(bp => bp.map(FarmBorder.matchingSegment))))
+        return ret;
+    })();
+
+    /**
+     * Determine if the two borders are directly connected
+     * @param other Border to check against
+     * @param directionToOther Rotational direction from this to other
+     * @returns if the two borders are connection
+     */
+    connected(other: FarmBorder, directionToOther: number): boolean {
+        // Get the tile intersection
+        const segs = FarmBorder.directionalSides[directionToOther];
+
+        // Verify that segments are on tile intersection
+        const orig = this.getSegments().filter(s => segs[0].includes(s));
+        const dest = other.getSegments().filter(s => segs[1].includes(s));
+        return orig.some(s => dest.includes(FarmBorder.matchingSegment(s)))
+    }
+
+    /**
+     * Direction vector for tiles to check
+     * @virtual
+     */
+    tileDirections(): boolean[] {
+        // TODO this gives false positives! and is super inefficient.. should make it pure virtual
+        return [
+            ...this.getSegments().map(s => Math.floor(s)),
+            ...this.getSegments().map(s => Math.ceil(s) % 4),
+        ].reduce((v, d) => {
+            v[d] = true;
+            return v;
+        }, new Array(4).fill(false));
+    }
+
+    /**
+     * Used to check if a rotational angle falls within the domain of this farm
+     * @param rotation rotational position to use
+     */
+    contains(rotation: number): boolean {
+        return this.getSegments().some(seg => seg === rotation || (seg + 0.5) % 4 === rotation)
+    }
 }
 
 /**
@@ -75,7 +130,7 @@ export class FarmBorderArea extends FarmBorder {
     /**
      * All connected segments
      */
-    segments: FarmBorderSegment[]
+    private segments: FarmBorderSegment[]
 
     /**
      * @param tile relevant tile
@@ -104,8 +159,23 @@ export class FarmBorderArea extends FarmBorder {
         }
     }
 
-    includes(segment: FarmBorderSegment) {
-        return this.segments.includes(segment);
+    getSegments() {
+        return this.segments
+    }
+
+    /**
+     * @override
+     */
+    tileDirections() {
+        let start = Math.ceil(this.segments[0]);
+        const end = Math.floor(this.segments[this.segments.length - 1]);
+        const ret = [false, false, false, false];
+        while (start != end) {
+            ret[start] = true;
+            start++;
+            start %= 4;
+        }
+        return ret;
     }
 };
 
@@ -118,12 +188,12 @@ export class FarmBorderPassageway extends FarmBorder {
     /**
      * Connection side a of passageway
      */
-    a: FarmBorderArea;
+    private a: FarmBorderArea;
 
     /**
      * Connection side b of passageway
      */
-    b: FarmBorderArea;
+    private b: FarmBorderArea;
 
     /**
      * @param tile relevant tile
@@ -136,7 +206,19 @@ export class FarmBorderPassageway extends FarmBorder {
         this.b = new FarmBorderArea(tile, b, rotation);
     }
 
-    includes(segment: FarmBorderSegment) {
-        return this.a.includes(segment) || this.b.includes(segment);
+    getSegments() {
+        return this.a.getSegments().concat(this.b.getSegments());
+    }
+
+    /**
+     * @override
+     */
+    tileDirections() {
+        const adirs = this.a.tileDirections();
+        const bdirs = this.b.tileDirections();
+        for (let i = 0; i < 4; i++)
+            if (bdirs[i])
+                adirs[i] = true;
+        return adirs;
     }
 };
--- lib/Table.ts
+++ lib/Table.ts
@@ -1,21 +1,82 @@
 import Tile from "./Tile";
 import SparseMatrix from './ds/SparseMatrix';
 import Follower, { FollowerType } from "./Follower";
+import { FarmBorder } from "./FarmBorder";
 
 // Where the tiles get placed
 export default class Table {
     // Location Map
     grid: SparseMatrix<Tile>;
 
+    /**
+     * Find spots where a tile could be placed
+     * @returns array of x,y coordinates
+     */
+    private getEmptySpots(): [number, number][] {
+        // TOOD this should be replaced with an iterative system
 
+        // Flood-fill algorithm
+        return (function rec(x, y, ret: [number, number][], seen) {
+            // Base case
+            if (seen.get(x, y))
+                return ret;
+            seen.set(x, y, true);
+
+            // If tile is empty
+            const tile = this.grid.get(x, y);
+            if (!tile) {
+                ret.push([x, y]);
+                return ret;
+            }
+
+            // Branch out
+            rec(x, y + 1, ret, seen);
+            rec(x + 1, y, ret, seen);
+            rec(x, y - 1, ret, seen);
+            rec(x - 1, y, ret, seen);
+            return ret;
+        })(0, 0, [], new SparseMatrix<boolean>());
+    }
+
+
+    /**
+     * Can we place given tile as specified?
+     * @param tile tile object
+     * @param x x coordinate
+     * @param y y coordinate
+     * @param rotation how much to rotate it
+     * @returns true if can place false if not
+     */
     canPlaceTile(tile: Tile, x: number, y: number, rotation: number) {
+        // Get neighboring borders
+        const neighbors = [
+            this.grid.get(x, y + 1),
+            this.grid.get(x + 1, y),
+            this.grid.get(x, y - 1),
+            this.grid.get(x - 1, y),
+        ].map((n, i) =>
+            n && n.getBorders()[(i + 2) % 4]);
 
+        // Check if borders line up
+        const borders = tile.getBorders(rotation);
+        for (let i = 0; i < 4; i++)
+            if (neighbors[i] && borders[i] !== neighbors[i])
+                return false;
+        return true;
     }
 
-    legalMoves(tile: Tile) {
-
+    /**
+     * Returns a set of ways the tile can be paced
+     * @param tile tile that has to be placed
+     */
+    legalPlacements(tile: Tile) {
+        const ret: [number, number, number][] = [];
+        for (const [x, y] of this.getEmptySpots())
+            for (let rotation = 0; rotation < 4; rotation++)
+                if (this.canPlaceTile(tile, x, y, rotation))
+                    ret.push([x, y, rotation]);
+        return ret;
     }
-
 
     /**
      * Get a valid follower types for this location
@@ -23,43 +84,63 @@ export default class Table {
      * @param y y position
      * @param tile
      */
-    validFollowerTypes(x: number, y: number, tile: Tile = this.grid.get(x, y), rotation: number = 0) {
+    validFollowers(x: number, y: number, tile: Tile = this.grid.get(x, y), rotation?: number) {
         // Unique tile elements
         const tileBorders = tile.getBorders(rotation);
 
         // Check conditions for monk
-        const ret: FollowerType[] = [];
+        const ret: Follower[] = [];
         if (tile.hasCloister())
-            ret.push(Follower.Type.MONK);
+            ret.push(new Follower(Follower.Type.MONK, tile));
 
         // Check conditions for thief
         if (tileBorders.includes(Tile.Border.ROAD)) {
             const roadBorders = tileBorders.map(b => b === Tile.Border.ROAD);
-            if (!this.roadOccupied(x, y, roadBorders))
-                ret.push(Follower.Type.THIEF);
+            if (!this.roadOccupied(x, y, roadBorders)) {
+                // List of road directions
+                const directions = roadBorders
+                    .map((b, i) => b ? i : null)
+                    .filter(e => e !== null)
+
+                // Add relevant follower options
+                if (directions.length <= 2)
+                    ret.push(new Follower(Follower.Type.THIEF, tile, directions[0]));
+                else
+                    ret.push(...directions.map(d => new Follower(Follower.Type.THIEF, tile, d)));
+            }
+
         }
 
         // Check conditions for knight
         if (tileBorders.includes(Tile.Border.CITY)) {
             const cityBorders = tileBorders.map(b => b === Tile.Border.CITY);
             if (tile.cityConnected()) {
-                if (this.cityOccupied(x, y, cityBorders))
-                    ret.push(Follower.Type.KNIGHT);
+                if (!this.cityOccupied(x, y, cityBorders))
+                    ret.push(new Follower(Follower.Type.KNIGHT, tile, cityBorders.indexOf(true)));
             } else {
                 cityBorders.forEach((b, i) => {
                     if (b) {
                         const directions = [false, false, false, false];
                         directions[i] = true;
                         if (this.roadOccupied(x, y, directions))
-                            ret.push(Follower.Type.KNIGHT)
+                            ret.push(new Follower(Follower.Type.KNIGHT, tile, i));
                     }
                 })
             }
         }
 
-        // TODO knight condition
-        //    similar to thief but uses
-        // TODO farmer condition
+        // Check conditions for farmer
+        if (tileBorders.includes(Tile.Border.ROAD) || tileBorders.includes(Tile.Border.FARM)) {
+            const farms = tile.farmConnections(rotation);
+            farms.forEach(farm => {
+                if (!this.farmOccupied(x, y, farm)) {
+                    const segs = farm.getSegments();
+                    ret.push(new Follower(Follower.Type.FARMER, tile, segs[1] || segs[0]));
+                }
+            });
+        }
+
+        return ret;
     }
 
     /**
@@ -71,6 +152,8 @@ export default class Table {
      * @returns if the connecting road-network already has a thief
      */
     roadOccupied(x: number, y: number, directions: boolean[], set: Set<Tile> = new Set()): boolean {
+        // TODO this might be incorrect behavior with respect to intersections
+
         // Get the borders of the tile which have roads, excluding previous tile
         const getRoadBorders = (t: Tile, i: number) => {
             const badIdx = (i + 2) % 4;
@@ -78,8 +161,7 @@ export default class Table {
         };
 
         // Check neighbors
-        // DFS for a thief
-        for (let i = 0; i < directions.length; i++) {
+        for (let i = 0; i < 4; i++) {
             // Skip if not a road border
             if (!directions[i])
                 continue;
@@ -124,7 +206,7 @@ export default class Table {
         };
 
         // Check neighbors
-        for (let i = 0; i < directions.length; i++) {
+        for (let i = 0; i < 4; i++) {
             // Skip if not a road border
             if (!directions[i])
                 continue;
@@ -140,8 +222,11 @@ export default class Table {
             if (set.has(tile))
                 continue;
 
-            // Tile has a theif
-            if (tile.getFollowers().some(f => f.type === Follower.Type.KNIGHT && (f.position === i || tile.cityConnected())))
+            // Tile occupied by knight
+            const hasKnight = tile.getFollowers().some(f =>
+                f.type === Follower.Type.KNIGHT
+                && (f.position === ((i + 2) % 4) || tile.cityConnected()));
+            if (hasKnight)
                 return true;
 
             // We can't DFS neighbors because it's not connected
@@ -154,11 +239,53 @@ export default class Table {
                 return true;
         }
 
-        // Road network unoccupied
+        // City unoccupied
         return false;
     }
 
-    farmOccupied(x: number, y: number, ) {
+    /**
+     * Is this farm area already designated to a farmer?
+     * @param x starting x coordinate
+     * @param y starting y coordinate
+     * @param farm farmborder to use
+     * @param set set to prevent looping
+     */
+    farmOccupied(x: number, y: number, farm: FarmBorder, set = new Set()) {
+        // Check neighbors
+        const directions = farm.tileDirections();
+        for (let i = 0; i < 4; i++) {
+            if (!directions[i])
+                continue;
 
+            // Get tile
+            const neighborX = [0, 1, 0, -1][i] + x;
+            const neighborY = [1, 0, -1, 0][i] + y;
+            const tile = this.grid.get(neighborX, neighborY);
+
+            // Skip empty or already seen tiles
+            if (!tile)
+                continue;
+            if (set.has(tile))
+                continue;
+
+            // Skip if other tile's farms not connected
+            const connFarm = tile.farmConnections().find(f => farm.connected(f, i));
+            if (!connFarm)
+                continue;
+
+            // Already have a farmer
+            const occupied = tile.getFollowers().some(f =>
+                f.type === Follower.Type.FARMER && connFarm.contains(f.position))
+            if (occupied)
+                return true;
+
+            // DFS other connected farms
+            set.add(tile);
+            if (this.farmOccupied(neighborX, neighborY, connFarm, set))
+                return true;
+        }
+
+        // Unoccupied
+        return false;
     }
 };
